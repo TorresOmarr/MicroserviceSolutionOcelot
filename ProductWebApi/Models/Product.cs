52a49952a096462cf9ddfd123d809112ac14f777
Omar Torres <jtorres@axsistec.com>
2023-05-11 16:56:18 -0600

    product web api

---
+++ ProductWebApi/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using MySqlX.XDevAPI;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductWebApi.Models
+{
+    [Table("Product", Schema = "dbo")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("product")]
+        public int ProductId { get; set; }
+        [Column("product_name")]
+        public string ProductName { get; set; } = null!;
+        [Column("product_cocd")]
+        public int ProductCode { get; set; }
+        [Column("product_price")]
+        public decimal ProductPrice { get; set; }
+        asdas
+    }
+}
