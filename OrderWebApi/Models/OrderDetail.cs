a08f6cfdc5a906912968ca6e0d760f0c2d5fe54d
Omar Torres <jtorres@axsistec.com>
2023-05-12 16:47:00 -0600

    implementando bd mongo con docker compose y actualizando  OrderDetail Entity

--- OrderWebApi/Models/OrderDetail.cs
+++ OrderWebApi/Models/OrderDetail.cs
@@ -5,7 +5,7 @@ namespace OrderWebApi.Models
 {
     public class OrderDetail
     {
-        [BsonElement("product_id"), BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("product_id"), BsonRepresentation(BsonType.Int32)]
         public int ProductId { get; set; }
         [BsonElement("quantity"), BsonRepresentation(BsonType.Decimal128)]
         public decimal Quantity { get; set; }
