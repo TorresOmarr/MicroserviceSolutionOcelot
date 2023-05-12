857d0a377e235045ae6e7f869ed6362c11b7ddd6
Omar Torres <jtorres@axsistec.com>
2023-05-12 10:28:18 -0600

    Api Order Created

---
+++ OrderWebApi/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderWebApi.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class Order
+    {
+        [BsonId, BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string OrderId { get; set; } = null!;
+        [BsonElement("customer_id"), BsonRepresentation(BsonType.ObjectId)]
+        public int CustomerId { get; set; }
+        [BsonElement("ordered_on"), BsonRepresentation(BsonType.ObjectId)]
+        public DateTime OrderedOn { get; set; }
+        [BsonElement("order_details")]
+        public List<OrderDetail> OrderDetails { get; set; }  
+    }
+
+}
---
+++ OrderWebApi/Models/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace OrderWebApi.Models
+{
+    public class OrderDetail
+    {
+        [BsonElement("product_id"), BsonRepresentation(BsonType.ObjectId)]
+        public int ProductId { get; set; }
+        [BsonElement("quantity"), BsonRepresentation(BsonType.ObjectId)]
+        public decimal Quantity { get; set; }
+        [BsonElement("unit_price"), BsonRepresentation(BsonType.ObjectId)]
+        public decimal UnitPrice { get; set; }
+    }
+}
